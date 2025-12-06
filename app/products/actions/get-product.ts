aedf73346860675cfd61e44da4dfdec091829d76
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2025-12-06 11:07:30 +0300

    feat: enhance get function to support URL search parameters for product fetching

--- app/products/actions/get-product.ts
+++ app/products/actions/get-product.ts
@@ -4,5 +4,9 @@ import { get } from "@/app/common/utils/fetch";
 import { Product } from "../interfaces/products.interface";
 
 export default async function getProducts() {
-  return get<Product[]>("products", ["products"]);
+  return get<Product[]>(
+    "products",
+    ["products"],
+    new URLSearchParams({ status: "available" })
+  );
 }
